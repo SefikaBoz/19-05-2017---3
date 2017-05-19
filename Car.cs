54c17a04279aa6db17bbc4e2936db7ecba574207
SefikaBoz <sefikaboz12@gmail.com>
2017-05-19 18:38:51 +0300

    Create Car.cs

---
+++ Car.cs
@@ -0,0 +1,25 @@
+# 19-05-2017---3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedAndInterface
+{
+    class Car : IEquatable<Car>
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public bool Equals(Car car)
+        {
+            if (this.Make == car.Make && this.Model == car.Model && this.Year == car.Year)
+                return true;
+            else
+                return false;
+        }
+    }
+}
